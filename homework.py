d5dfa337f10be25ab15eae3d2bc3b4dd912990c4
Антон Кузнецов <Skayzer8@yandex.ru>
2022-06-06 17:29:46 +0300

    edit exceptions, loggers scope, add settings.py file

--- homework.py
+++ homework.py
@@ -6,34 +6,13 @@ from http import HTTPStatus
 import requests
 import telegram
 from dotenv import load_dotenv
+import settings
 
 load_dotenv()
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='%(asctime)s, %(funcName)s, %(levelname)s, %(message)s',
-    handlers=[logging.FileHandler(
-        'main.log',
-        mode='w',
-        encoding='UTF-8'),
-        logging.StreamHandler()])
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-
-RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-
-
-HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
-}
 
 
 def send_message(bot, message):
@@ -41,8 +20,9 @@ def send_message(bot, message):
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.info('Сообщение в чат {TELEGRAM_CHAT_ID}: {message}')
-    except Exception:
-        logger.error('Сообщение в телеграм не отправилось')
+    except telegram.TelegramError:
+        logger.error('Сообщение в телеграм не отправилось.')
+        raise telegram.TelegramError
 
 
 def get_api_answer(current_timestamp):
@@ -53,19 +33,20 @@ def get_api_answer(current_timestamp):
     """
     timestamp = current_timestamp
     params = {'from_date': timestamp}
+    REQUEST_API = {
+        'url': settings.ENDPOINT,
+        'headers': settings.HEADERS,
+        'params': params,
+    }
     try:
-        homework_statuses = requests.get(
-            ENDPOINT,
-            headers=HEADERS,
-            params=params
-        )
-    except Exception as error:
+        homework_statuses = requests.get(**REQUEST_API)
+    except telegram.TelegramError as error:
         logging.error(f'Ошибка запроса к  API: {error}')
-        raise Exception(f'Ошибка запроса к  API: {error}')
+        raise telegram.TelegramError(f'Ошибка запроса к  API: {error}')
     if homework_statuses.status_code != HTTPStatus.OK:
         status_code = homework_statuses.status_code
         logging.error(f'Ошибка доступности  API: {status_code}')
-        raise Exception(f'Ошибка доступности  API: {status_code}')
+        raise telegram.TelegramError(f'Ошибка доступности  API: {status_code}')
     return homework_statuses.json()
 
 
@@ -99,12 +80,13 @@ def parse_status(homework):
     if 'homework_name' not in homework:
         raise KeyError('Не найден ключ "homework_name" в ответе от сервера')
     if 'status' not in homework:
-        raise Exception('Не найден ключ "status" в ответе от сервера')
+        raise KeyError('Не найден ключ "status" в ответе от сервера')
     homework_name = homework['homework_name']
     homework_status = homework['status']
-    if homework_status not in HOMEWORK_STATUSES:
-        raise Exception(f'Недокументированный статус работы:{homework_status}')
-    verdict = HOMEWORK_STATUSES[homework_status]
+    if homework_status not in settings.HOMEWORK_STATUSES:
+        raise telegram.TelegramError(f'Недокументированный статус'
+                                     f'работы:{homework_status}')
+    verdict = settings.HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -126,7 +108,7 @@ def main():
     new_message = ''
     if not check_tokens():
         logger.critical('Проверка наличия токенов провалена')
-        raise Exception('Проверка наличия токенов провалена')
+        raise telegram.error.InvalidToken('Проверка наличия токенов провалена')
     while True:
         try:
             response = get_api_answer(current_timestamp)
@@ -136,15 +118,25 @@ def main():
             if message != new_message:
                 send_message(bot, message)
                 new_message = message
-                time.sleep(RETRY_TIME)
+                time.sleep(settings.RETRY_TIME)
         except Exception as error:
             logger.error(error)
             error_message = str(error)
             if error_message != new_error_message:
                 send_message(bot, error_message)
                 new_error_message = error_message
-        time.sleep(RETRY_TIME)
+        time.sleep(settings.RETRY_TIME)
 
 
 if __name__ == '__main__':
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format='%(asctime)s, %(funcName)s, %(levelname)s, %(message)s',
+        handlers=[logging.FileHandler(
+            'main.log',
+            mode='w',
+            encoding='UTF-8'),
+            logging.StreamHandler()])
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
     main()
